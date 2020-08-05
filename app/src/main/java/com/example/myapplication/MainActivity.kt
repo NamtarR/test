dd023dbece1449cd6623a936189e3875ff322264
Anton Tarasov <namtarr123@gmail.com>
2020-08-05 11:22:44 +0700

    Добавил новый тестовый фрагмент

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.ui.DarkFragment
+import com.example.myapplication.ui.LightFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainMenu.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuLight -> openLight()
+                R.id.menuDark -> openDark()
+            }
+            true
+        }
+    }
+
+    fun openLight() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, LightFragment())
+            .commit()
+    }
+
+    fun openDark() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, DarkFragment())
+            .commit()
     }
 }
